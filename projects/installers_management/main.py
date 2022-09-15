38fd5340b74ae2f71aeb33965a8ab3dea245f9c3
Biumsp <enricobus98@gmail.com>
2022-09-15 15:01:12 +0200

    Add requirements

--- projects/installers_management/main.py
+++ projects/installers_management/main.py
@@ -3,17 +3,23 @@ from rules import rules
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 
-# define the scope
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+def main(request):
+    # define the scope
+    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
-# add credentials to the account
-creds = ServiceAccountCredentials.from_json_keyfile_name('/home/bus/dev/gsheets/credentials.json', scope)
+    # add credentials to the account
+    creds = ServiceAccountCredentials.from_json_keyfile_name('/home/bus/dev/gsheets/credentials.json', scope)
 
-# authorize the clientsheet 
-client = gspread.authorize(creds)
+    # authorize the clientsheet 
+    client = gspread.authorize(creds)
 
-# get the instance of the Spreadsheet
-spreadsheet_installers = client.open_by_url('https://docs.google.com/spreadsheets/d/10Z31s0rlv-LNQzrMrI-SOl6TPkmoiF0CZYqpYTv277U/edit#gid=1655103781')
+    # get the instance of the Spreadsheet
+    spreadsheet_installers = client.open_by_url('https://docs.google.com/spreadsheets/d/1t9OwWtZHR96VsnzgBSAk1eVHeUVsBMybcEWmW4WgGqA/edit#gid=382931988')
 
-# execute all rules
-for rule in rules['spreadsheet_installers']: rule(spreadsheet_installers)
+    # execute all rules
+    for rule in rules['spreadsheet_installers']: rule(spreadsheet_installers)
+
+    return 200
+
+if __name__=='__main__':
+    main('')
