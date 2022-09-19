31fc3dda866d937004bcfaf75a8939797ce38956
Biumsp <enricobus98@gmail.com>
2022-09-19 11:51:25 +0200

    Correct Main

--- projects/installers_management/main.py
+++ projects/installers_management/main.py
@@ -8,25 +8,16 @@ def main(request):
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
     # add credentials to the account
-    start = time.time()
     creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
-    print("ServiceAccount = ", time.time()-start)
-    start = time.time()
 
     # authorize the clientsheet 
     client = gspread.authorize(creds)
-    print("Client = ", time.time()-start)
-    start = time.time()
 
     # get the instance of the Spreadsheet
     spreadsheet_installers = client.open_by_url('https://docs.google.com/spreadsheets/d/1t9OwWtZHR96VsnzgBSAk1eVHeUVsBMybcEWmW4WgGqA/edit#gid=382931988')
-    print("Spreadsheet = ", time.time()-start)
-    start = time.time()
 
     # execute all rules
     for rule in rules['spreadsheet_installers']: rule(spreadsheet_installers)
-    print("Rules = ", time.time()-start)
-    start = time.time()
 
     return 200
 
