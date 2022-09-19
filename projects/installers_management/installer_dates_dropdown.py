b37891ba8f8a6dab91d5e825b6ba00eedef9547b
Biumsp <enricobus98@gmail.com>
2022-09-19 11:28:25 +0200

    Improve performance

--- projects/installers_management/installer_dates_dropdown.py
+++ projects/installers_management/installer_dates_dropdown.py
@@ -1,10 +1,12 @@
 from rule import Rule
 from gsheets_update_cell import GSheetsUpdateCell
 from gsheets_update import GSheetsUpdate
-import re
+import re, time
 
 
 def installer_dates_dropdown(spreadsheet):
+
+    COLUMN_DATES = 15
     
     # Read the date worksheet
     sheet_dates = spreadsheet.worksheet('CALENDAR INSTALLERS')
@@ -23,45 +25,48 @@ def installer_dates_dropdown(spreadsheet):
         dates.update({installer.strip().lower(): installer_dates})
 
     # Read the projects worksheet
-    sheet_projects = spreadsheet.worksheet('Copy of MAIN - Mike')
+    sheet_projects = spreadsheet.worksheet('Test Enrico')
 
     # Get all projects
     all_projects = sheet_projects.col_values(2)
+    projects_dates = sheet_projects.col_values(COLUMN_DATES)
 
     # Create the update object
     update = GSheetsUpdate()
 
+    partial = 0
     # Get dates to be updated and modify available dates
     for installer in dates:
 
-        selected  = []
-        to_update = []
+        selected  = []  # List of already selected dates for this installer
+        to_update = []  # List of rows where there is a dropdown list to update
 
         # List of row indexes of projects with the same name
-        projects_rows = [i+1 for i, p in enumerate(all_projects) if p.strip().lower() == installer]
+        projects_rows = [i for i, p in enumerate(all_projects) if p.strip().lower() == installer]
 
         for row in projects_rows:
-            project_date_cell = sheet_projects.cell(row, 16)
-            project_date = project_date_cell.value
+            try: project_date = projects_dates[row]
+            except: break
 
             if project_date in dates[installer]:
                 selected.append(project_date)
-            else:
-                to_update.append(project_date_cell)
+
+            to_update.append(row)
 
         # Remove selected dates from available dates
         for s in selected:
-            dates[installer].remove(s)
+            try: dates[installer].remove(s)
+            except: pass
 
-        # Remove duplicates
+        # Remove duplicates and sort
         dates[installer] = list(set(dates[installer]))
         dates[installer].sort(key=lambda x: x[-4::]+x[3:5]+x[0:2])
 
-        for c in to_update:
+        for row in to_update:
             # Create the  object
             request = GSheetsUpdateCell('dropdown')
 
-            request.range(c.row-1, c.row, c.col-1, c.col)
+            request.range(row, row+1, COLUMN_DATES-1, COLUMN_DATES)
             request.sheet_id(sheet_projects.id)
             request.value_list(dates[installer])
 
--- projects/installers_management/main.py
+++ projects/installers_management/main.py
@@ -1,23 +1,32 @@
 # importing the required libraries
 from rules import rules
 from oauth2client.service_account import ServiceAccountCredentials
-import gspread
+import gspread, time
 
 def main(request):
     # define the scope
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
     # add credentials to the account
+    start = time.time()
     creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+    print("ServiceAccount = ", time.time()-start)
+    start = time.time()
 
     # authorize the clientsheet 
     client = gspread.authorize(creds)
+    print("Client = ", time.time()-start)
+    start = time.time()
 
     # get the instance of the Spreadsheet
     spreadsheet_installers = client.open_by_url('https://docs.google.com/spreadsheets/d/1t9OwWtZHR96VsnzgBSAk1eVHeUVsBMybcEWmW4WgGqA/edit#gid=382931988')
+    print("Spreadsheet = ", time.time()-start)
+    start = time.time()
 
     # execute all rules
     for rule in rules['spreadsheet_installers']: rule(spreadsheet_installers)
+    print("Rules = ", time.time()-start)
+    start = time.time()
 
     return 200
 
